18b0303e0bed13258be3cd7f71345cbe75466a28
mfdunlap <maya.dunlap@slu.edu>
2023-09-21 13:21:42 -0500

    Update calculate_dof.py
    - Change phrasing to ask for m
    - Change dofs to ask how many joints have a certain number of DoFs
    - Edit error checking for m and J
    - Add comments

--- Lab5/calculate_dof.py
+++ Lab5/calculate_dof.py
@@ -1,23 +1,32 @@
 def main():
+    # Initialize variables
     quit_prog = False
-    m = None
-    N = None
-    J = None
-    joint_dofs = list()
+    m      = None
+    N      = None
+    J      = None
+    j_1dof = None
+    j_2dof = None
+    j_3dof = None
 
+    # Make program runnable for multiple calculations
     while not quit_prog:
+        # Get m (number of DoF for a single body)
         while not m:
             m = get_dimensions()
         
+        # Get N (number of links)
         while not N:
             N = get_links()
 
-        while not J or not joint_dofs:
-            J, joint_dofs = get_joints()
+        # Get J (number of joints) and number of joints w/ 1, 2, and 3 DoF
+        while not any([J, j_1dof, j_2dof, j_3dof]):
+            J, j_1dof, j_2dof, j_3dof = get_joints()
 
-        mech_dof = m * (N - 1 - J) + sum(joint_dofs)
+        # Calculate mechanism DoF
+        mech_dof = m * (N - 1 - J) + j_1dof + 2 * (j_2dof) + 3 * (j_3dof)
         print(f"This is a {mech_dof} DOF mechanism.")
-        
+
+        # Ask if user wants to make another DoF calculation
         cont = input("To calculate another mechanism's DOF press enter. To quit the program type 'q' or 'quit'. ").lower()
         if cont in ["q", "quit"]:
             quit_prog = True
@@ -25,18 +34,23 @@ def main():
             m = None
             N = None
             J = None
-            joint_dofs = list()
+            j_1dof = None
+            j_2dof = None
+            j_3dof = None
 
 
 def get_dimensions():
-    dimensions = input("Is this mechanism operating in 2D or 3D space: ").lower()
-    if dimensions == "2d":
-        return 3
-    elif dimensions == "3d":
-        return 6
-    else:
-        print("ValueError: Please answer with either '2D' or '3D'.")
+    try:
+        dimensions = int(input("How many DoFs does a single body have: "))
+        if dimensions not in [3, 6]:
+            raise ValueError()
+        else:
+            return dimensions
+        
+    except ValueError:
+        print("Error: Single bodies can only have 3 (2D space) or 6 (3D space) DoF.")
         return
+
     
 
 def get_links():
@@ -45,30 +59,30 @@ def get_links():
         return n_links
 
     except ValueError:
-        print("ValueError: All values must be integers.")
+        print("Error: All values must be integers.")
         return None
 
 
 def get_joints():
     try:
-        n_joints = int(input("How many joints does this mechanism have: "))
-        # TODO: MAKE JOINTS_DOFS A LIST OF ZEROS THAT HAS 
-        joint_dofs = [0 for i in range(n_joints)]
-        
-        for n in range(n_joints):
-            while joint_dofs[n] == 0:
-                joint_dof = input(f"How many DOFs does joint {n+1} have: ")
-                if joint_dof not in ["1", "2", "3"]:
-                    print("ValueError: Individual joints must have DOF values of 1, 2, or 3.")
-                else:
-                    joint_dofs[n] = int(joint_dof)
+        joints_valid = False
+        while not joints_valid:
+            n_joints = int(input("How many joints does this mechanism have: "))
+            n_1dof   = int(input("How many joints have 1 DoF: "))
+            n_2dof   = int(input("How many joints have 2 DoF: "))
+            n_3dof   = int(input("How many joints have 3 DoF: "))
+            
+            if n_1dof + n_2dof + n_3dof != n_joints:
+                print("Error: The meachanism's total number of joints must equal the sum of 1, 2, and 3 DoF joints")
+            else:
+                joints_valid = True
     
     except ValueError:
-        print("ValueError: All values must be integers.")
-        return None, None
+        print("Error: All values must be integers.")
+        return None, None, None, None
     
 
-    return n_joints, joint_dofs
+    return n_joints, n_1dof, n_2dof, n_3dof
 
 
 #################### TEST/RUN CODE ####################
