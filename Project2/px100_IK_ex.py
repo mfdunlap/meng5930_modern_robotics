f0235dece4fe369a6be64c6b86f90abd67059ce0
Maya Dunlap <maya.dunlap@slu.edu>
2023-12-06 08:18:54 -0600

    Update project 2

--- Project2/px100_IK_ex.py
+++ Project2/px100_IK_ex.py
@@ -2,6 +2,7 @@ from interbotix_xs_modules.xs_robot.arm import InterbotixManipulatorXS
 from math import atan2, sqrt, pi, acos, sin, cos, asin
 from scipy.linalg import logm, expm
 import numpy as np
+from time import sleep
 
 class ourAPI:
     def __init__(self):
@@ -11,14 +12,14 @@ class ourAPI:
         self.Lm = 0.035
         self.L3 = 0.100
         self.L4 = 0.1076
-        self.S = np.array([[0, 0, 1, 0,       0,     0],
-                           [0, 1, 0, 0, -0.0895,     0],
-                           [0, 1, 0, 0, -0.1895, 0.035],
-                           [0, 1, 0, -0.1895, 0, 0.135]]) # Screw axes
-        self.M = np.array([[1, 0, 0,  0.2426],
-                           [0, 1, 0,       0],
-                           [0, 0, 1, 0.18945],
-                           [0, 0, 0,       1]]) # End-effector M matrix
+        self.S = np.array([[0, 0, 1,                  0, 0,               0],
+                           [0, 1, 0,           -self.L1, 0,               0],
+                           [0, 1, 0, -(self.L1+self.L2), 0,         self.Lm],
+                           [0, 1, 0, -(self.L1+self.L2), 0, self.Lm+self.L3]]) # Screw axes
+        self.M = np.array([[1, 0, 0, self.Lm+self.L3+self.L4],
+                           [0, 1, 0,                       0],
+                           [0, 0, 1,         self.L1+self.L2],
+                           [0, 0, 0,                       1]]) # End-effector M matrix
         
     def screw_axis_to_transformation_matrix(self, screw_axis, angle):
         """
@@ -64,8 +65,8 @@ class ourAPI:
         """
         assert twist_matrix.shape == (4, 4), "Input matrix must be 4x4"
 
-        w = np.array([[twist_matrix[2,1], twist_matrix[0,2], twist_matrix[1,0]]])
-        v = np.array([twist_matrix[:3,-1]])
+        w = np.array([twist_matrix[2,1], twist_matrix[0,2], twist_matrix[1,0]])
+        v = np.array(twist_matrix[:3,-1])
 
         return np.concatenate((w, v), axis=None)
     
@@ -80,6 +81,8 @@ class ourAPI:
         
         # Calculate adjoint of Tbs
         Tsb     = self.fk_poe(angles)
+        # print("Tsb:")
+        # print(Tsb)
         Tbs     = np.linalg.inv(Tsb)
         R       = Tbs[:3, :3]
         p       = Tbs[:3, -1]
@@ -88,22 +91,18 @@ class ourAPI:
                            [-p[1],  p[0],     0]])
         
         adj_Tbs_top = np.hstack((R, np.zeros((3, 3))))
-        adj_Tbs_bottom = np.hstack((np.dot(p_brack, R), R))
+        adj_Tbs_bottom = np.hstack((p_brack @ R, R))
         adj_Tbs = np.vstack((adj_Tbs_top, adj_Tbs_bottom))
 
         # Convert space jacobian into body jacobian
-        J = adj_Tbs @ J
-        return J
-    
-    def poe(self, angles):
+        Jb = adj_Tbs @ J
+        return Tbs, Jb
+
+    def fk_poe(self, angles):
         T = np.eye(4)
         for i in range(len(angles)):
             Ti = self.screw_axis_to_transformation_matrix(self.S[i], angles[i])
             T  = T @ Ti
-        return T
-    
-    def fk_poe(self, angles):
-        T = self.poe(angles)
         T = T @ self.M
         return T
 
@@ -151,38 +150,81 @@ class ourAPI:
         """
         Gives joint angles using numerical method.
         """
-        for i in range(100):
+        for i in range(10000):
             # Calculate the end-effector transform (Tsb) evaluated at the InitGuess using the helper functions that you wrote at the beginning.
-            Tsb = self.fk_poe(InitGuess)
+            Tbs, Jb = self.body_jacobian(InitGuess)
 
             # Compute the body twist
-            matrix_Vb = logm(np.linalg.inv(Tsb) @ Tsd)
-            print(matrix_Vb)
+            matrix_Vb = logm(Tbs @ Tsd)
             Vb = self.twist_vector_from_twist_matrix(matrix_Vb)     # use the helper function at the beginning to extract the vector
-            print(Vb)
-
+            
             # Compute new angles
-            Jb = self.body_jacobian(InitGuess)
             NewGuess = InitGuess + np.linalg.pinv(Jb) @ Vb
             print(f"Iteration number: {i} \n")
 
             # Check if you're done and update initial guess
-            if(np.linalg.norm(abs(NewGuess-InitGuess)) <= 0.001):
+            if(np.linalg.norm(abs(NewGuess-InitGuess)) <= 0.1):
+                print("NEW GUESS:")
+                print(NewGuess)
                 return [NewGuess[0], NewGuess[1], NewGuess[2], NewGuess[3]] 
             else:
                 InitGuess = NewGuess
         print('Numerical solution failed!!')
+        print("Tbs:")
+        print(Tbs)
+        print("Jb:")
+        print(Jb)
+        print("NEW GUESS:")
+        print(NewGuess)
 
 def main():
-    # Determine the desired end-effector transform
-    Td_grasp = np.array([[1, 0, 0,  0],
-                         [0, 1, 0,  0],
-                         [0, 0, 1, 0.10],
-                         [0, 0, 0,  1]]) # Gripping location
-    Td_release = np.array([[1, 0, 0, 0.05],
-                           [0, 1, 0, 0.05],
-                           [0, 0, 1, 0.2],
-                           [0, 0, 0,  1]]) # Throwing location
+    # Move robot to grasping position
+    Td_1 = np.array([[ 0.0, 1.0, 0.0,     0.0],
+                     [-1.0, 0.0, 0.0, -0.2426],
+                     [ 0.0, 0.0, 1.0, 0.18945],
+                     [ 0.0, 0.0, 0.0,     1.0]])
+    
+    Td_2 = np.array([[ 0.0, 1.0, 0.0,     0.5],
+                     [-1.0, 0.0, 0.0, -0.2426],
+                     [ 0.0, 0.0, 1.0, 0.03945],
+                     [ 0.0, 0.0, 0.0,     1.0]])
+    
+    # Lift item
+    Td_3 = np.array([[ 0.0, 1.0, 0.0,     0.6],
+                     [-1.0, 0.0, 0.0, -0.2426],
+                     [ 0.0, 0.0, 1.0, 0.23945],
+                     [ 0.0, 0.0, 0.0,     1.0]])
+    
+    
+    # Rotate item
+    Td_4 = np.array([[       0.0, 1.0,      -0.0,         0.0],
+                     [      -0.5, 0.0, 0.8660254, -0.03469746],
+                     [ 0.8660254, 0.0,       0.5,  0.34954776],
+                     [       0.0, 0.0,       0.0,         1.0]])
+    
+    # Put item down
+    Td_5 = np.array([[      0.5, -0.8660254, 0.0,     0.1213],
+                     [0.8660254,        0.5, 0.0, 0.21009776],
+                     [      0.0,        0.0, 1.0,    0.18945],
+                     [      0.0,        0.0, 0.0,        1.0]])
+    
+    Td_6 = np.array([[      0.5, -0.8660254, 0.0,     0.1813],
+                     [0.8660254,        0.5, 0.0, 0.21009776],
+                     [      0.0,        0.0, 1.0,    0.18945],
+                     [      0.0,        0.0, 0.0,        1.0]])
+    
+    Td_7 = np.array([[      0.5, -0.8660254, 0.0,     0.1813],
+                     [0.8660254,        0.5, 0.0, 0.21009776],
+                     [      0.0,        0.0, 1.0,    0.00945],
+                     [      0.0,        0.0, 0.0,        1.0]])
+    
+    # Move away from released item
+    Td_8 = np.array([[      0.5, -0.8660254, 0.0,     0.1813],
+                     [0.8660254,        0.5, 0.0, 0.21009776],
+                     [      0.0,        0.0, 1.0,    0.20945],
+                     [      0.0,        0.0, 0.0,        1.0]])
+    
+
 
     # Create experiment objects (use robot API + our custom API)
     bot = InterbotixManipulatorXS(
@@ -195,20 +237,124 @@ def main():
     # Start with home positiongeom_IK
     bot.arm.go_to_home_pose()
 
-    # toggle between the geometric method and the numerical method below
-    # record the answers that you get in both cases. report your observations. 
+    # Choose geometric or numerical IK solver
+    method = ""
+    while not method:
+        method = input("Geometric (g) or Numerical (n): ").lower()
+        method = method.strip()
+        if method not in ["g", "n"]:
+            method = ""
+        elif method == "g":
+            print("Geometric method selected...")
+        elif method == "n":
+            print("Numerical method selected...")
 
-    # Go to gripping position and grip
-    #joint_positions = my_api.geom_IK(Td_grasp) # Geometric inverse kinematics
-    joint_positions = my_api.num_IK(Td_grasp, np.array([0.0, 0.0, 0.0, 0.0])) # Numeric inverse kinematics
-    bot.arm.set_joint_positions(joint_positions) # Set positions
-    bot.gripper.grasp(2.0) # Grip
+    # Move robot with IK
+    if method == "g":
+        # Move to grasp position
+        print(f"Finding position 1...")
+        joint_positions = my_api.geom_IK(Td_1)
+        bot.arm.set_joint_positions(joint_positions)
+        sleep(1)
+
+        print(f"Finding position 2...")
+        joint_positions = my_api.geom_IK(Td_2)
+        bot.arm.set_joint_positions(joint_positions)
+        sleep(1)
+
+        # Grip item
+        bot.gripper.set_pressure(0.75)
+        bot.gripper.grasp(2.0)
+
+        # Lift item
+        print(f"Finding position 3...")
+        joint_positions = my_api.geom_IK(Td_3)
+        bot.arm.set_joint_positions(joint_positions)
+        sleep(1)
+
+        # Rotate item
+        print(f"Finding position 4...")
+        joint_positions = my_api.geom_IK(Td_4)
+        bot.arm.set_joint_positions(joint_positions)
+        sleep(1)
+
+        # Lower item
+        print(f"Finding position 5...")
+        joint_positions = my_api.geom_IK(Td_5)
+        bot.arm.set_joint_positions(joint_positions)
+        sleep(1)
+
+        print(f"Finding position 6...")
+        joint_positions = my_api.geom_IK(Td_6)
+        bot.arm.set_joint_positions(joint_positions)
+        sleep(1)
+
+        print(f"Finding position 7...")
+        joint_positions = my_api.geom_IK(Td_7)
+        bot.arm.set_joint_positions(joint_positions)
+        sleep(1)
+
+        # Release item
+        bot.gripper.release(2.0)
+
+        # Move away from item
+        print(f"Finding position 8...")
+        joint_positions = my_api.geom_IK(Td_8)
+        bot.arm.set_joint_positions(joint_positions)
+        sleep(1)
     
-    # Go to throwing position and throw
-    #joint_positions = my_api.geom_IK(Td_release) # Geometric inverse kinematics
-    joint_positions = my_api.num_IK(Td_release, np.array([0.0, 0.0, 0.0, 0.0])) # Numeric inverse kinematics
-    bot.arm.set_joint_positions(joint_positions) # Set positions
-    bot.gripper.release(2.0) # Release
+    else:
+        # Move to grasp position
+        print(f"Finding position 1...")
+        joint_positions = my_api.num_IK(Td_1, np.array([0.0, 0.0, 0.0, 0.0]))
+        bot.arm.set_joint_positions(joint_positions)
+        sleep(1)
+
+        print(f"Finding position 2...")
+        joint_positions = my_api.num_IK(Td_2, np.array([0.0, 0.0, 0.0, 0.0]))
+        bot.arm.set_joint_positions(joint_positions)
+        sleep(1)
+
+        # Grip item
+        bot.gripper.set_pressure(0.75)
+        bot.gripper.grasp(2.0)
+
+        # Lift item
+        print(f"Finding position 3...")
+        joint_positions = my_api.num_IK(Td_3, np.array([0.0, 0.0, 0.0, 0.0]))
+        bot.arm.set_joint_positions(joint_positions)
+        sleep(1)
+
+        # Rotate item
+        print(f"Finding position 4...")
+        joint_positions = my_api.num_IK(Td_4, np.array([0.0, 0.0, 0.0, 0.0]))
+        bot.arm.set_joint_positions(joint_positions)
+        sleep(1)
+
+        # Lower item
+        print(f"Finding position 5...")
+        joint_positions = my_api.num_IK(Td_5, np.array([0.0, 0.0, 0.0, 0.0]))
+        bot.arm.set_joint_positions(joint_positions)
+        sleep(1)
+
+        print(f"Finding position 6...")
+        joint_positions = my_api.num_IK(Td_6, np.array([0.0, 0.0, 0.0, 0.0]))
+        bot.arm.set_joint_positions(joint_positions)
+        sleep(1)
+
+        print(f"Finding position 7...")
+        joint_positions = my_api.num_IK(Td_7, np.array([0.0, 0.0, 0.0, 0.0]))
+        bot.arm.set_joint_positions(joint_positions)
+        sleep(1)
+
+        # Release item
+        bot.gripper.release(2.0)
+
+        # Move away from item
+        print(f"Finding position 8...")
+        joint_positions = my_api.num_IK(Td_8, np.array([0.0, 0.0, 0.0, 0.0]))
+        bot.arm.set_joint_positions(joint_positions)
+        sleep(1)
     
     # End mission
     bot.arm.go_to_home_pose()
